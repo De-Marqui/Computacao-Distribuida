a1b6e4bcb71adccc715c49206fb343b7a6eeb6d2
Victor De Marqui <victor.demarqui@gmail.com>
2022-10-31 23:49:58 -0300

    Update Exercício1.py

--- nuvem/Exercício1.py
+++ nuvem/Exercício1.py
@@ -1,1 +1,23 @@
+from flask import Flask
+import requests
+app = Flask(__name__)
 
+@app.route("/",methods=['GET'])
+def loadBalancer():
+  return "Success", 200
+@app.route('/convertemoeda/<valor>',methods=['GET'])
+
+def convertemoeda(valor):
+  url = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL"
+  response = requests.get(url)
+  response_json = response.json()
+  dolar = response_json.get("USDBRL").get("low")
+  euro = response_json.get("EURBRL").get("low")
+  usd = float(dolar)
+  eur = float(euro)
+  real = float(valor)
+  converteResultado = f"[Real -> Dolar: {real/usd}]" + f"[Real -> Euro: {real/eur}]" 
+  return converteResultado
+
+if __name__ == '__main__':
+ app.run()
